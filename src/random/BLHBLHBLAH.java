fe37f09597d2d9923a28a074675a09e99fd1b8d1
Grace <33816722+gracepluo@users.noreply.github.com>
2017-12-13 19:57:06 -0800

    Add files via upload

---
+++ src/random/BLHBLHBLAH.java
@@ -0,0 +1,13 @@
+package random;
+
+import java.util.Random;
+
+public class BLHBLHBLAH {
+public static void main(String[] args) {
+	Random bb8 = new Random ();
+	int num = bb8.nextInt(123456789);
+	System.out.println(num);
+	
+}
+	
+}
--- src/random/Validation.java
+++ src/random/Validation.java
@@ -12,14 +12,26 @@ public class Validation {
 
 		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
 		
-		int randomNumber = new Random().nextInt(5);
+		int randomChoice = new Random().nextInt(5);
 		
 		// 2. On paper, write all the numbers that get printed when you run this class 10 times
 		
-		System.out.println(randomNumber);
+		System.out.println(randomChoice);
 
 		// 3. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomChoice == 6) {
+			System.out.println("hi");
+		} else if (randomChoice == 5) {
+			System.out.println("Looks ok");
+		} else if (randomChoice == 4) {
+		System.out.println("XD");
+		} else if (randomChoice == 3) {
+			System.out.println("No");
+		} else if (randomChoice == 2) {
+			System.out.println("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeEEEeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeEEEEeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEeeeeeeeeeeeeeeeeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEET");
+		} else {
+			System.out.println("Yay~");
+		}
 		// 4. Repeat all the code above 10 times
 		
 		// 5. Find someone to test out your program. They will like it :)
